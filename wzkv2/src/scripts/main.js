772eb405182386e6c383f34c1872b2fa6831045b
Zenetics1 <williamzanderkim@gmail.com>
2024-11-05 17:30:35 -0500

    Removed Stuff

--- wzkv2/src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-var Typed = new Typed('.typeTxt', {
-    strings: ["a Dreamer", "an Innovator", "a Team player"],
-    typeSpeed: 200,
-    backSpeed: 175,
-    backDelay: 1000,
-    loop: true
-});
