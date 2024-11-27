64daefe4f0601869714cf8015f0281884a3691ce
Zenetics1 <williamzanderkim@gmail.com>
2024-11-27 01:59:30 -0500

    Fixed

--- wzkv2/astro.config.mjs
+++ wzkv2/astro.config.mjs
@@ -5,16 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   integrations: [tailwind(), react()],
   prefetch: true,
   site: 'https://wizk.ca',
   base: '/wzkv2/',
-=======
-=======
->>>>>>> parent of addd7d8 (Added Sidebar)
-  integrations: [tailwind()],
-  prefetch: true
->>>>>>> parent of addd7d8 (Added Sidebar)
+
 });
