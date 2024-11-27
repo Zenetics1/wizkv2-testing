d5a0d98e0090998bb34491bcafb73188e54a55be
Zenetics1 <williamzanderkim@gmail.com>
2024-11-26 23:42:30 -0500

    Setting up website

--- wzkv2/astro.config.mjs
+++ wzkv2/astro.config.mjs
@@ -8,5 +8,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  prefetch: true
+  prefetch: true,
+  site: 'https://wizk.ca',
 });
