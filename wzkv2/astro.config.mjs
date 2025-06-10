7e5fbf15873b50fcefa818582ae19cdce5eae5d6
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2025-06-10 12:12:07 -0400

    reset fixes

--- wzkv2/astro.config.mjs
+++ wzkv2/astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
 
   integrations: [tailwind(), react()],
   prefetch: true,
   site: 'https://Zenetics.github.io',
+=======
+  integrations: [tailwind()],
+  prefetch: true
+>>>>>>> parent of addd7d8 (Added Sidebar)
 });
